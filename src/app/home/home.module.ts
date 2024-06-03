9314ff8e9c656367810c838132ce86f6c5881b6b
Luis Castaño <luisjcu99@gmail.com>
2024-06-03 12:42:14 +0200

    instalar ng-material

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { HomeRoutingModule } from './home-routing.module';
-
 import { HomeComponent } from './home.component';
 import { SharedModule } from '../shared/shared.module';
 
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, SharedModule, HomeRoutingModule]
+  imports: [CommonModule, SharedModule, HomeRoutingModule, MatSlideToggleModule]
 })
 export class HomeModule {}
